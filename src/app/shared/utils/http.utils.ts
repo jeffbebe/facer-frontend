e202d7bcb807a224e3a1a4ce802075719874c1a6
jeffbebe <oskarw46@gmail.com>
2022-01-05 19:52:41 +0100

    feat: modify error service to properly display errors

--- src/app/shared/utils/http.utils.ts
+++ src/app/shared/utils/http.utils.ts
@@ -5,10 +5,11 @@ export const HTTP_STATUS = {
   UNAUTHORIZED: 401,
   FORBIDDEN: 403,
   NOT_FOUND: 404,
+  METHOD_NOT_ALLOWED: 405,
   CLIENT_CLOSED_REQUEST: 499,
   INTERNAL_SERVER_ERROR: 500,
   SERVER_ERROR_UNKNOWN: 599,
 } as const;
 
 export const isUnhandledError = (status: number): boolean =>
-  status >= HTTP_STATUS.UNAUTHORIZED;
+  status >= HTTP_STATUS.METHOD_NOT_ALLOWED;
