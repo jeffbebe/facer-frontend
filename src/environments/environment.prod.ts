d20b01505cbb9affac4b9b4dd75e4f1579f58475
jeffbebe <oskarw46@gmail.com>
2022-01-28 12:20:34 +0100

    some minor fixes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  region: 'eu-central-1',
+  userPoolId: 'eu-central-1_hNoqG8mo2',
+  userPoolWebClientId: '6vu0cev9vp78h1stjafjf762b6',
+  backendUrl: 'https://facer.cloud/api',
 };
